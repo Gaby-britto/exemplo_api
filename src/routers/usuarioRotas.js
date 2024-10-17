20fdd8a66b79b2d18d51ecfed6ee8e189d8180d9
Gaby Brito <154287645+Gaby-britto@users.noreply.github.com>
2024-10-17 14:40:23 -0300

    add middlewares

--- src/routers/usuarioRotas.js
+++ src/routers/usuarioRotas.js
@@ -1,12 +1,13 @@
 const { Router } = require('express');
 const userController = require('../controllers/userController')
 const router = Router();
+const {validateUser, validateUserId} = require('../middlewares/validateUser');
 
-router.post('/', userController.create );
-router.put('/:id', userController.update );
-router.delete('/:id', userController.delete );
-router.get('/:id', userController.getOne );
-router.get('/', userController.getAll );
+router.post('/', validateUser, userController.create );
+router.put('/:id', validateUser, validateUserId, userController.update );
+router.delete('/:id', validateUser, validateUserId, userController.delete );
+router.get('/:id', validateUser, validateUserId,userController.getOne );
+router.get('/', validateUser, userController.getAll );
 
 module.exports = router;
 
